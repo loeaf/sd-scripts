174d22470440edbcffa10052036fa01f65cf8359
loeaf <ckdic8524@naver.com>
2025-01-07 14:46:46 +0900

    싱글이미지 구현

--- create-single-img.py
+++ create-single-img.py
@@ -46,12 +46,12 @@ class FontImageGenerator:
             draw.text(position, text, font=font, fill='black')
 
             # 이미지와 텍스트 파일 저장
-            image_path = os.path.join(self.output_dir, f"image{index}.png")
-            txt_path = os.path.join(self.output_dir, f"image{index}.txt")
+            image_path = os.path.join(self.output_dir, f"{index}_{text}.png")
+            # txt_path = os.path.join(self.output_dir, f"image{text}.txt")
 
             image.save(image_path)
-            with open(txt_path, 'w', encoding='utf-8') as f:
-                f.write(f"English character {text}, font character")
+            # with open(txt_path, 'w', encoding='utf-8') as f:
+            #     f.write(f"English character {text}, font character")
 
         except Exception as e:
             print(f"폰트 '{font_path}' 처리 중 오류 발생: {str(e)}")
@@ -62,14 +62,20 @@ def main():
     generator = FontImageGenerator()
 
     # 영문자 배열 생성
-    # arr_eng = []
+    # arr_eng = []ㅌㅌ
     # for i in range(65, 91):  # A-Z
     #     arr_eng.append(chr(i))
     # for i in range(97, 123):  # a-z
     #     arr_eng.append(chr(i))
     # for 가
     # 각 문자에 대해 이미지 생성
-    generator.create_font_image('를', "./481d2eaca08bef8b2e833324477d674d", 0)  # 폰트 경로는 실제 경로로 수정 필요
+    # generator.create_font_image('현', "./481d2eaca08bef8b2e833324477d674d", 0)  # 폰트 경로는 실제 경로로 수정 필요
+    # 얇은거
+    # 뭉게구름 노을 빛 하늘
+    data = ['뭉', '게', '구', '름', '노', '을', '빛', '하', '늘']
+    for index, data in enumerate(data):
+        # generator.create_font_image(data, "./db96f6e4dac8a65e529775aaa3d4dc27", index)
+        generator.create_font_image(data, "./481d2eaca08bef8b2e833324477d674d", index)
 
 
 if __name__ == "__main__":
