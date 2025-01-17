ad3d4bdff66987533dd642c71f16615327f205d1
loeaf <ckdic8524@naver.com>
2025-01-17 17:52:05 +0900

    update

--- train.py
+++ train.py
@@ -63,7 +63,7 @@ def main():
     # Process each dataset path
     for i, obj in enumerate(dataset_paths, 1):
         dataset_path = obj[0]
-        lora_path = obj[4].replace('\n', '')
+        lora_path = obj[3].replace('\n', '')
         # Base command without config path
         base_cmd = (
             f'CUDA_VISIBLE_DEVICES=1 {args.base_command} '
