d20d407b526d8fee2d8731a679cd5043d2791012
loeaf <ckdic8524@naver.com>
2025-02-10 15:44:50 +0900

    json import

--- train.py
+++ train.py
@@ -79,7 +79,7 @@ def main():
         # Base command without config path
         base_cmd = (
             f'CUDA_VISIBLE_DEVICES=1 {args.base_command} '
-            '--pretrained_model_name_or_path="/home/user/data/stable-diffusion-webui-forge/models/Stable-diffusion/sd3.5_large.safetensors" '
+            '--pretrained_model_name_or_path="/home/user/data/stable-diffusion-webui-forge/models/Stable-diffusion/flux1-dev.safetensors" '
             '--network_module=networks.lora '
             '--network_dim=64 '
             '--network_alpha=32 '
